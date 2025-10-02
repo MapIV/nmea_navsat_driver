2cecaeb3a73759d6c9a604926e5abf050839c7e4
hirotaka-mukumoto <hirotaka.mukumoto@map4.jp>
2025-10-02 09:20:58 +0000

    [feature][launch] add launch and config of serial_topic_reader, topic_driver

---
+++ launch/nmea_topic_serial_reader.launch.py
@@ -0,0 +1,42 @@
+
+""" A simple launch file for the nmea_topic_serial_reader node. """
+
+import os
+import sys
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription, LaunchIntrospector, LaunchService
+from launch_ros import actions
+
+
+def generate_launch_description():
+    """Generate a launch description for nmea_topic_serial_reader."""
+    config_file = os.path.join(get_package_share_directory("nmea_navsat_driver"), "config", "nmea_topic_serial_reader.yaml")
+    driver_node = actions.Node(
+        package='nmea_navsat_driver',
+        executable='nmea_topic_serial_reader',
+        output='screen',
+        parameters=[config_file])
+
+    return LaunchDescription([driver_node])
+
+
+def main(argv):
+    ld = generate_launch_description()
+
+    print('Starting introspection of launch description...')
+    print('')
+
+    print(LaunchIntrospector().format_launch_description(ld))
+
+    print('')
+    print('Starting launch of launch description...')
+    print('')
+
+    ls = LaunchService()
+    ls.include_launch_description(ld)
+    return ls.run()
+
+
+if __name__ == '__main__':
+    main(sys.argv)
