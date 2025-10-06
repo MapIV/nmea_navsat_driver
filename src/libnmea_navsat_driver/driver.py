58bb55824b479cddd72fe04b1c09e9a88a3955f3
hirotaka-mukumoto <hirotaka.mukumoto@map4.jp>
2025-10-06 08:56:07 +0000

    [feature][driver] add PASHR parser

--- src/libnmea_navsat_driver/driver.py
+++ src/libnmea_navsat_driver/driver.py
@@ -52,6 +52,7 @@ class Ros2NMEADriver(Node):
 
         self.time_ref_source = self.declare_parameter('time_ref_source', 'gps').value
         self.use_RMC = self.declare_parameter('useRMC', False).value
+        self.use_PASHR = self.declare_parameter('use_PASHR', False).value
         self.use_GNSS_time = self.declare_parameter('use_GNSS_time', False).value
         self.valid_fix = False
 
@@ -264,7 +265,7 @@ class Ros2NMEADriver(Node):
             self.lon_std_dev = data['lon_std_dev']
             self.lat_std_dev = data['lat_std_dev']
             self.alt_std_dev = data['alt_std_dev']
-        elif 'HDT' in parsed_sentence:
+        elif 'HDT' in parsed_sentence and not self.use_PASHR:
             data = parsed_sentence['HDT']
             if data['heading']:
                 current_heading = QuaternionStamped()
@@ -276,6 +277,20 @@ class Ros2NMEADriver(Node):
                 current_heading.quaternion.z = q[2]
                 current_heading.quaternion.w = q[3]
                 self.heading_pub.publish(current_heading)
+        elif 'SHR' in parsed_sentence and self.use_PASHR: # PASHR
+            data = parsed_sentence['SHR']
+            current_heading = QuaternionStamped()
+            current_heading.header.stamp = current_time
+            current_heading.header.frame_id = frame_id
+            q = quaternion_from_euler(
+                math.radians(data['roll']),
+                math.radians(data['pitch']),
+                math.radians(data['heading']))
+            current_heading.quaternion.x = q[0]
+            current_heading.quaternion.y = q[1]
+            current_heading.quaternion.z = q[2]
+            current_heading.quaternion.w = q[3]
+            self.heading_pub.publish(current_heading)
         else:
             return False
         return True
--- src/libnmea_navsat_driver/parser.py
+++ src/libnmea_navsat_driver/parser.py
@@ -196,6 +196,17 @@ parse_maps = {
     "HDT": [
         ("heading", safe_float, 1),
     ],
+    "SHR": [ # PASHR, dictionary key must be 3 characters
+        ("utc_time", convert_time, 1),
+        ("heading", safe_float, 2),
+        ("roll", safe_float, 4),
+        ("pitch", safe_float, 5),
+        ("roll_accuracy", safe_float, 7),
+        ("pitch_accuracy", safe_float, 8),
+        ("heading_accuracy", safe_float, 9),
+        ("gnss_quality", safe_int, 10),
+        ("imu_alignment_status", safe_int, 11)
+    ],
     "VTG": [
         ("true_course", convert_deg_to_rads, 1),
         ("speed", convert_knots_to_mps, 5)
@@ -205,8 +216,7 @@ parse_maps = {
 
 def parse_nmea_sentence(nmea_sentence):
     # Check for a valid nmea sentence
-
-    if not re.match(r'(^\$GP|^\$GN|^\$GL|^\$IN).*\*[0-9A-Fa-f]{2}$', nmea_sentence):
+    if not re.match(r'(^\$GP|^\$GN|^\$GL|^\$IN|^\$P).*\*[0-9A-Fa-f]{2}$', nmea_sentence):
         logger.debug("Regex didn't match, sentence not valid NMEA? Sentence was: %s"
                      % repr(nmea_sentence))
         return False
