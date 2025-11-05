240ccc021dba9fc0972bc742a27365a36a912a63
hirotaka-mukumoto <hirotaka.mukumoto@map4.jp>
2025-11-05 08:38:08 +0000

    [fix][driver] revert last commit because it was correct

--- src/libnmea_navsat_driver/driver.py
+++ src/libnmea_navsat_driver/driver.py
@@ -197,9 +197,7 @@ class Ros2NMEADriver(Node):
             current_fix.longitude = longitude
 
             # Altitude is above ellipsoid, so adjust for mean-sea-level
-            # 20251031 DON'T ADD mean-sea-level
-            #altitude = data['altitude'] + data['mean_sea_level']
-            altitude = data['altitude']
+            altitude = data['altitude'] + data['mean_sea_level']
             current_fix.altitude = altitude
 
             # use default epe std_dev unless we've received a GST sentence with epes
